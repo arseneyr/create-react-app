2605dfe44d9dfa0d851cae709b6e1832a7a14c3d
AR <arseneyr@users.noreply.github.com>
2019-11-19 12:14:48 -0800

    moving build path to root dir

--- packages/react-scripts/config/paths.js
+++ packages/react-scripts/config/paths.js
@@ -79,7 +79,7 @@ const resolveModule = (resolveFn, filePath) => {
 module.exports = {
   dotenv: resolveApp('.env'),
   appPath: resolveApp('.'),
-  appBuild: resolveApp('build'),
+  appBuild: resolveRoot('build/client'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveModule(resolveApp, 'index'),
@@ -102,7 +102,7 @@ const resolveOwn = relativePath => path.resolve(__dirname, '..', relativePath);
 module.exports = {
   dotenv: resolveApp('.env'),
   appPath: resolveApp('.'),
-  appBuild: resolveApp('build'),
+  appBuild: resolveRoot('build/client'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveModule(resolveApp, 'index'),
